d9f8c6ac37067cc541e09ebfffa711a187079c6b
Kp <70857174+10-kp@users.noreply.github.com>
2021-03-29 21:47:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
+const inquirer = require("inquirer");
+const fs = require ("fs");
+const jest = require("jest");
+const writeFileAsync = util.promisify(fs.writeFile);
 
+//import modules
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = requrite("./lib/Manager");
+
+//Empty array
+const allEmployees = [];
+
+
+//To ask user the questions
+const promptUser = () => {
+    return inquirer.prompt([
+      {
+       type: "list",
+       name: "role",
+       message: "What is the employee's role?",
+       choices: ["Manager", "Engineer", "Intern"], 
+      },
+      {
+        type: "text",
+        name: "employee",
+        message: "Name of the employee:",
+      }
+      {
+        type: "text",
+        name: "id",
+        message: "Please provide employee's ID number:",
+      }
+      {
+        type: "text",
+        name: "email",
+        message: "Please provide employee's email:",
+      }
+      
+      
+       
