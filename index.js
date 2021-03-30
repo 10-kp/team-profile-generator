b5a5c44ee030e1fa196535d404eba895a11b59f9
Kp <kp_1@outlook.com>
2021-03-30 05:52:21 +0400

    Start

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require ("fs");
+const jest = require("jest");
 
+//To ask user the questions
+const promptUser = () => {
+    return inquirer.prompt([  
+    
